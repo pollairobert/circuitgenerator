92974a72e8f0d0863c06afa230fd291bb73b91d6
pollairobert <pollairobert@gmail.com>
2019-09-01 14:08:03 +0200

    Express hozzaadasa es egy endpoint megvalositasa.(general egy 1-es tipusu aramkort)

---
+++ server.ts
@@ -0,0 +1,15 @@
+﻿import { CircuitGenerator } from './circuitgenerator';
+import { Circuit } from './circuit';
+
+var express=require('express');
+var app=express();
+app.get('/generate',function(req,res) {
+    let circuit = new CircuitGenerator();
+    
+    let response = circuit.generateCircuit(1);
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+var server=app.listen(3000,function() {
+    console.log('Listening....')
+});
